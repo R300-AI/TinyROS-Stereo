ffc6b582bd30268c49a8ed2ea6f2ac59d23972f1
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2025-07-11 13:23:31 +0800

    Update aligner.py

--- demo/aligner.py
+++ demo/aligner.py
@@ -9,18 +9,18 @@ aligner = StereoAligner(chessboard_size=(9, 6))
 os.makedirs('data/calibrate/left', exist_ok=True)
 os.makedirs('data/calibrate/right', exist_ok=True)
 
-def streaming(threadhold, state):
-    frameL, frameR = capL.read(), capR.read()
-    result = aligner.fit(frameL, frameR)
-    img = result.plot(threadhold=threadhold)
-    return gr.update(value=img), result
+def streaming(threadhold):
+    while True:
+        frameL, frameR = capL.read(), capR.read()
+        result = aligner.fit(frameL, frameR)
+        img = result.plot(threadhold=threadhold)
+        yield img, result
 
 with gr.Blocks() as demo:
     gr.Markdown("# Stereo Alignment Streaming (Gradio)")
     threadhold = gr.Number(value=2.0, label="Threadhold", precision=1, step=0.1)
     image = gr.Image(label="Stereo Merged")
     state = gr.State(None)
-    timer = gr.Timer(interval=0.1, repeat=True)
-    timer.tick(fn=streaming, inputs=[threadhold, state], outputs=[image, state])
+    demo.load(fn=streaming, inputs=[threadhold], outputs=[image, state])
 
 demo.launch(share=False, inbrowser=True)
